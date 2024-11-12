897d827b8229a5a896f58865e057a40be85d1f2a
sproutcake23 <75305540+sproutcake23@users.noreply.github.com>
2024-11-12 13:02:00 +0530

    Update main.py
    update code

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ data_cai_a = {
     "Friday": ["FIH", "C", "C", "C", "LIBRARY", "MFC", "MFC", "MFC"],
 }
 
+prio =[]
+not_prio=[]
+
 today = datetime.now()
 next_day_date = today + timedelta(days=1)
 next_day = next_day_date.strftime("%A")
@@ -17,24 +20,27 @@ next_day = next_day_date.strftime("%A")
 print("Current Day:", current_day)
 print("Next Day:", next_day)
 
-notDone = True # loop
-final_list = [] # final list
+notDone = True 
 
 while notDone:
     sub_input = input("Enter the subjects you need to work on :")
 
-    ## error -- cannot split
+    if sub_input.lower() == "q":
+        notDone =False
+        continue
+
     todo_sub=[x.strip() for x in sub_input.split(",")]
     
     next_day_tb = data_cai_a.get(next_day,[])
-    
-    if sub_input.lower() == "q":
-        final_list = todo_sub
-        print(sub_input.split(",")) ## DB
-        print(todo_sub) ## DB
 
-        notDone = False
+    for x in todo_sub:
+        if x in next_day_tb:
+            prio.append(x)
+        else:
+            not_prio.append(x)
+     
 
-for x in todo_sub:
-        print(f"Prioritise these subjects {x}")    
+print(f"Prioritise these subjects {prio}")           
+print(f"Do not prioritise these subjects {not_prio}")           
+         
     
